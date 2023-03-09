2f90e934fc2a771576448216232a020f1747857d
Firas Dhmaid <firasdhmaid@gmail.com>
2023-03-09 14:45:12 +0100

    signin and signup pages

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import {View , Text , StyleSheet} from 'react-native'
 
 import AuthPage from './screens/AuthPage';
+import Signin from './screens/signin';
+import SignUp from './screens/signup';
 
 function App() {
   return (
     <View>
-      <AuthPage />
+      <Signin/>
     </View>
   )
 }
